6e9a14f8968718d80e7f7fa6ec6059abd7e325e0
DELL <sunshuai@propersoft.cn>
2017-08-09 10:08:52 +0800

    增加了一个函数

--- src/com/sun/newfile.java
+++ src/com/sun/newfile.java
@@ -3,5 +3,9 @@ package com.sun;
 public class newfile {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		new newfile().disPlay();
+	}
+	public void disPlay(){
+		System.out.println("this is function disPlay();");
 	}
 }
