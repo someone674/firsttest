266a61df2d451283a73c44376464a7e9cdd991e8
DELL <sunshuai@propersoft.cn>
2017-08-09 10:14:19 +0800

    增加了一个判断函数

--- src/com/sun/newfile.java
+++ src/com/sun/newfile.java
@@ -4,8 +4,16 @@ public class newfile {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		new newfile().disPlay();
+		new newfile().isHello("Hello");
 	}
 	public void disPlay(){
 		System.out.println("this is function disPlay();");
+	}
+	public boolean isHello(String str){
+		if("Hello".equals(str)){
+			System.out.println("true");
+			return true;
+		}
+		return false;
 	}
 }
