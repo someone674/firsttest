b5333f14c352af599e5693cd722081023aa176e9
DELL <sunshuai@propersoft.cn>
2017-08-09 10:14:58 +0800

    去掉了展示函数

--- src/com/sun/newfile.java
+++ src/com/sun/newfile.java
@@ -3,11 +3,7 @@ package com.sun;
 public class newfile {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		new newfile().disPlay();
 		new newfile().isHello("Hello");
-	}
-	public void disPlay(){
-		System.out.println("this is function disPlay();");
 	}
 	public boolean isHello(String str){
 		if("Hello".equals(str)){
