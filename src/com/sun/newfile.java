ec1e9455b9366b04d5798b450bc997d9b3eab09c
someone674 <sunshuai@propersoft.cn>
2017-08-09 11:23:41 +0800

    添加一个开始输出

--- src/com/sun/newfile.java
+++ src/com/sun/newfile.java
@@ -4,6 +4,7 @@ public class newfile {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		new newfile().isHello("Hello");
+		System.out.println("start");
 		System.out.println("end");
 	}
 	public boolean isHello(String str){
@@ -13,4 +14,5 @@ public class newfile {
 		}
 		return false;
 	}
+
 }
